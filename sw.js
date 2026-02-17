6cd9c79d1dc8ab910f5a8980f9c312d105a2a7f8
crisneyfreire <crisneyfreire@gmail.com>
2026-02-16 21:23:42 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+// sw.js - Service Worker para cache offline
+const CACHE_NAME = 'gastos-v1';
+const urlsToCache = [
+  '/',
+  'appfinanceiro.html',
+  'manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
